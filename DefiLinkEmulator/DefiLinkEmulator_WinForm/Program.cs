351159d3271d7b5370b5ac4e365e1f0c170f4e0a
sz2 <sugiura.k.nigo@gmail.com>
2019-12-21 13:45:23 +0900

    Modify DefiLinkEmulator_WinFotm to dotnetcoreapp3.1

--- DefiLinkEmulator/DefiLinkEmulator_WinForm/Program.cs
+++ DefiLinkEmulator/DefiLinkEmulator_WinForm/Program.cs
@@ -7,12 +7,10 @@ namespace DefiLinkEmulator
 {
     static class Program
     {
-        /// <summary>
-        /// アプリケーションのメイン エントリ ポイントです。
-        /// </summary>
         [STAThread]
         static void Main()
         {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new DefilinkEmulator_WinForm());
