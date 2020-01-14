d5dee605fa7370baba7043f5a4a6ffeaa4c05f64
sz2 <sugiura.k.nigo@gmail.com>
2020-01-15 00:07:53 +0900

    Create signalR template.

--- DefiLinkEmulator/DefiArduinoEmulator_WebApp/Startup.cs
+++ DefiLinkEmulator/DefiArduinoEmulator_WebApp/Startup.cs
@@ -7,33 +7,28 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using DefiArduinoEmulator_WebApp.Hubs;
 
-namespace DefiArduinoEmulator_WebApp
-{
-    public class Startup
-    {
+namespace DefiArduinoEmulator_WebApp {
+    public class Startup {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddSignalR ();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
             }
 
-            app.UseRouting();
+            app.UseRouting ();
+            app.UseDefaultFiles ();
+            app.UseStaticFiles ();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+            app.UseEndpoints (endpoints => {
+                endpoints.MapHub<ChatHub> ("/hub");
             });
         }
     }
