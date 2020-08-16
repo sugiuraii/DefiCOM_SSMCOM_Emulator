a07dfa43200f134d54903413f108156efd5b1c3d
sz2 <sugiura.k.nigo@gmail.com>
2020-08-16 20:10:47 +0900

    Add UI componet reference.

--- DefiLinkEmulator/DefiLinkEmulator_GtkSharp/MainWindow.cs
+++ DefiLinkEmulator/DefiLinkEmulator_GtkSharp/MainWindow.cs
@@ -6,8 +6,16 @@ namespace DefiLinkEmulator_GtkSharp
 {
     class MainWindow : Window
     {
-        [UI] private Label _label1 = null;
-        [UI] private Button _button1 = null;
+        [UI] private Entry comPortNameEntry = null;
+        [UI] private Scale boostScale = null;
+        [UI] private Scale engRevScale = null;
+        [UI] private Scale oilPresScale = null;
+        [UI] private Scale fuelPresScale = null;
+        [UI] private Scale extTempScale = null;
+        [UI] private Scale oilTempScale = null;
+        [UI] private Scale waterTempScale = null;
+        [UI] private Button startButton = null;
+
 
         private int _counter;
 
@@ -18,18 +26,19 @@ namespace DefiLinkEmulator_GtkSharp
             builder.Autoconnect(this);
 
             DeleteEvent += Window_DeleteEvent;
-            _button1.Clicked += Button1_Clicked;
+            //_button1.Clicked += Button1_Clicked;
         }
 
         private void Window_DeleteEvent(object sender, DeleteEventArgs a)
         {
             Application.Quit();
         }
-
+/*
         private void Button1_Clicked(object sender, EventArgs a)
         {
             _counter++;
             _label1.Text = "Hello World! This button has been clicked " + _counter + " time(s).";
         }
+*/
     }
 }
